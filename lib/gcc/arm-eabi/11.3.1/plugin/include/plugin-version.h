00dc558e04fedcdb63d4efcae97f8843d1250a94
Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>
2022-12-01 16:01:54 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/, 01122022 build.             GCC VERSION:             GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/             BUILDER COMMIT: 70e4ce9ad051228a989f6cc4798ef0041e04c6c2

--- lib/gcc/arm-eabi/11.3.1/plugin/include/plugin-version.h
+++ lib/gcc/arm-eabi/11.3.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.3.1";
-static char datestamp[] = "20221130";
+static char datestamp[] = "20221201";
 static char devphase[] = "";
 static char revision[] = "";
 
