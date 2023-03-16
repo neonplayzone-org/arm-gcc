d3aeb204b6809b641fe446bedbf7d2c01b601546
Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>
2023-03-16 19:39:13 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/, 16032023 build.             GCC VERSION:             GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/             BUILDER COMMIT: e4d643e12a3496a080c6787607a86b43d368df20

--- lib/gcc/arm-eabi/12.2.1/plugin/include/version.h
+++ lib/gcc/arm-eabi/12.2.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.2.1 20230309"
+#define version_string "12.2.1 20230316"
 #define pkgversion_string "(NEON GCC) "
 
 /* This is the location of the online document giving instructions for
